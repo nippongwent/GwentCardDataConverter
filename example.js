ccc1c216f47e8ce14ae1fb4ee8d59e2e86da22c9
nippongwent <54656913+nippongwent@users.noreply.github.com>
2021-12-27 14:55:09 +0900

    CSV変換機能を追加したので、使用例も更新した

--- example.js
+++ example.js
@@ -1,4 +1,10 @@
 const GwentCards = require('./lib/GwentCards');
+const Converter = require('./lib/converter/GwentCard2CSV');
 
-let gc = new GwentCards("v.9.6.1");
-console.log(gc.getJSON());
+let version = 'v.9.6.1';
+let lang    = 'ja-jp';
+
+let gc  = new GwentCards(version);
+let CSV = new Converter(gc);
+
+CSV.saveFile(`./cards_${version}_${lang}.csv`, lang);
