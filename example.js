1f82cb5842eb158050d0128d27019d51d32b40f2
nippongwent <54656913+nippongwent@users.noreply.github.com>
2023-09-15 16:52:56 +0900

    v.11.9
    追加隠し状態「tainted」と「ale」はアドホックに解決

--- example.js
+++ example.js
@@ -2,7 +2,7 @@ const GwentCards = require('./lib/GwentCards');
 const ConverterCSV = require('./lib/converter/GwentCard2CSV');
 const ConverterSS  = require('./lib/converter/GwentCard2Seesaa');
 
-let version = 'v.11.8.0';
+let version = 'v.11.9.0';
 let lang    = 'ja-jp';
 
 let gc  = new GwentCards(version);
