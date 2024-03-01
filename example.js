d6ca430b7471be26877ce33c7ce44bc25b4586d7
nippongwent <54656913+nippongwent@users.noreply.github.com>
2024-03-01 16:44:07 +0900

    v.bc20240301.5

--- example.js
+++ example.js
@@ -1,16 +1,16 @@
 // グウェントフィニティ以降は
 // 修正データはクライアントにないので新規作成せず、
-// GwentCards.jsonに公式サイトデッキビルダーのAPIから得られた最新情報をパッチとしてダウンロードしておく
+// GwentCards.jsonに公式サイトデッキビルダーのAPIから得られた最新情報をパッチとして適用する
 // dataディレクトリには前バージョンで作成したGwentCards.jsonが入っている必要がある
 
-// const GwentCards = require('./lib/GwentCards');
+// const GwentCards = require('./lib/GwentCards'); // クライアントデータを抽出する場合はこちら
 const GwentCards = require('./lib/GwentfinityCards');
 const ConverterCSV = require('./lib/converter/GwentCard2CSV');
 const ConverterSS = require('./lib/converter/GwentCard2Seesaa');
-const { execSync } = require('child_process'); // パッチを落としてから作業を始めるために必要
+const { execSync } = require('child_process'); // パッチを落としてから作業を始めるための同期処理に必要
 
 // グウェントフィニティ以後のバージョンは「v.bc（日付）.（回数）」の形式でないとエラーに
-let version = 'v.bc20231115.2';
+let version = 'v.bc20240301.5';
 
 // 先にパッチをダウンロードしておかなければならない
 const stdout = execSync(`node PatchDownloader.js ${version}`);
